cbfbd4bce6ba29c706435a953690736d8423f6d0
MartijnSchippers <martijn.schippers2@gmail.com>
2017-08-05 19:27:26 +0200

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,15 @@
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+FPS = 60
+
+WIDTH = 400
+HEIGHT = 600
+TITLE = 'smikkel'
+
+ALL_KAASJES = [(50, 10, True), (200, 40, False), (340, 5, True), (500, 75, True)]
